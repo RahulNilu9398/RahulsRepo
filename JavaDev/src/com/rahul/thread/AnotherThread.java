094b271bc1b14f0815b70af0e9101a2092f91833
RahulNilu9398 <33790128+rahulnilu9398@users.noreply.github.com>
2017-11-19 01:15:24 +0530

    Initial commit1

--- JavaDev/src/com/rahul/thread/AnotherThread.java
+++ JavaDev/src/com/rahul/thread/AnotherThread.java
@@ -1,6 +1,6 @@
 package com.rahul.thread;
 
-public class AnotherThread {
+public class AnotherThread extends Thread {
     public static void main(String[] args){
         System.out.println("Hi from another thread");
     }
--- JavaDev/src/com/rahul/thread/FirstThread.java
+++ JavaDev/src/com/rahul/thread/FirstThread.java
@@ -4,5 +4,10 @@ public class FirstThread {
     public static void main(String[] args){
         System.out.println("Helo from First Thread");
 
+        Thread anotherThread=new AnotherThread();
+        anotherThread.start();
+
+        System.out.println("Hello again from First Thread");
+
     }
 }
