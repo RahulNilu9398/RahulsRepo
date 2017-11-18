bccd8ee6f5b52713005eadbf8264b0255a5a3aef
RahulNilu9398 <33790128+RahulNilu9398@users.noreply.github.com>
2017-11-19 01:01:00 +0530

    Initial commit

---
+++ JavaDev/src/com/rahul/thread/AnotherThread.java
@@ -0,0 +1,7 @@
+package com.rahul.thread;
+
+public class AnotherThread {
+    public static void main(String[] args){
+        System.out.println("Hi from another thread");
+    }
+}
---
+++ JavaDev/src/com/rahul/thread/FirstThread.java
@@ -0,0 +1,8 @@
+package com.rahul.thread;
+
+public class FirstThread {
+    public static void main(String[] args){
+        System.out.println("Helo from First Thread");
+
+    }
+}
